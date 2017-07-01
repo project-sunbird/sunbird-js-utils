37bbf2606b607282047ec14082bfd09e38e658b1
anuj <anujtech46>
2017-07-01 17:51:45 +0530

    Issue #1 feat: Add logger implementation in create note service

--- sb_logger_util/index.js
+++ sb_logger_util/index.js
@@ -4,21 +4,40 @@
  * @desc: using log4s, enables application wide logging.
  */
 var log4js = require('log4js');
-var log4js_extend = require('log4js-extend');
 var path = require('path');
+var fs = require('fs');
+var dir = '../../log';
 
-log4js_extend(log4js, {
-    path: __dirname,
-    format: "at @name (@file:@line:@column)"
-});
+if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+}
 
 var logdir = path.join(__dirname, '../../');
 var logconfig = require('./log4js.json', { cwd: logdir });
 log4js.configure(logconfig);
 var logger = log4js.getLogger('api');
 
-var getLogger = function() {
-    return logger;
+var info = function (data) {
+    logger.setLevel('INFO');
+    logger.info(JSON.stringify(data));
 };
 
-exports.logger = getLogger();
+var error = function (data) {
+    logger.setLevel('ERROR');
+    logger.error(JSON.stringify(data));
+};
+
+var warn = function (data) {
+    logger.setLevel('WARN');
+    logger.warn(JSON.stringify(data));
+};
+
+var trace = function (data) {
+    logger.setLevel('TRACE');
+    logger.trace(JSON.stringify(data));
+};
+
+module.exports.info = info;
+module.exports.error = error;
+module.exports.warn = warn;
+module.exports.trace = trace;
