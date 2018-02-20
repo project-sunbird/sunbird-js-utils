1890a38e6fa0621e7e4287fee6085f0bf9137167
anuj <anujtech46>
2018-02-20 16:58:30 +0530

    Issue #SB-1721 fix: Added page assemble api

--- sb-config-util/index.js
+++ sb-config-util/index.js
@@ -12,7 +12,8 @@ var config_data = {
     LEARNER_SERVICE_BASE_URL: "https://dev.open-sunbird.org/api",
     LEARNER_SERVICE_AUTHORIZATION_TOKEN: '',
     LS_SEND_EMAIL: '/user/v1/notification/email',
-    LS_HEALTH_CHECK: "/user/v1/health"
+    LS_HEALTH_CHECK: "/user/v1/health",
+    LEARNER_SERVICE_PAGE_ASSEMBLE: "/data/v1/page/assemble"
 };
 
 setConfig = function (name, value) {
