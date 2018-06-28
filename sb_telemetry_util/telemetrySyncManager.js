a0a5670d91c25c93780b093663d37f4dfcb644bc
Harishkumar Gangula <harishg@ilimi.in>
2018-06-28 16:17:07 +0530

    Issue #SB-4509 fix:  telemetry updated status codes for retry

--- sb_telemetry_util/telemetrySyncManager.js
+++ sb_telemetry_util/telemetrySyncManager.js
@@ -101,8 +101,8 @@ telemetrySyncManager.prototype.sync = function (events, callback) {
         callback(null, res)
       }
 
-      if(res &&  _.includes([503, 502, 429, 401],res.statusCode)) {
-        callback(new Error('error while syncing telemetry with code'), null);
+      if(res &&  _.includes([503, 502, 429, 401], res.statusCode)) {
+        callback(new Error('Error while syncing telemetry with code: '+res.statusCode), null);
       } else {
         console.log('Error while syncing telemetry', events);
         callback(null, events);
