e08d13adb3344f0700b36936ff34ff12b6f1a968
yravinderkumar33 <yravinderkumar33@gmail.com>
2019-02-06 23:59:18 +0530

    refactored changes

--- sb_logger_util_v2/index.js
+++ sb_logger_util_v2/index.js
@@ -2,18 +2,18 @@ const log4js = require('log4js');
 var mkdirp = require('mkdirp');
 const fs = require('fs');
 const path = require('path');
-let loggerInit = false;
+let loggerInitialized = false;
 let config;
 
-const init = (newConfig) => {
+const init = (appConfig) => {
 
     const defaultConfig = {
-        path: '../logs',
+        path: '../logs/microservice.log',
         enableLogger: true,
         logLevel: 'error'
     }
 
-    config = { ...defaultConfig, ...newConfig }
+    config = { ...defaultConfig, ...appConfig }
 
     if (config.path) {
         mkdirp.sync(path.dirname(config.path));
@@ -35,14 +35,14 @@ const init = (newConfig) => {
         }
     });
 
-    loggerInit = true;
+    loggerInitialized = true;
 }
 
 var processRequestObject = (request) => {
     if (request) {
         return {
             id: request.id,
-            path: request.route.path,
+            path: request.path,
             method: request.method
         }
     } else {
@@ -53,43 +53,43 @@ var processRequestObject = (request) => {
 const logger = log4js.getLogger('api');
 
 var info = function (data, request) {
-    if (loggerInit && config.enableLogger) {
+    if (loggerInitialized && config.enableLogger) {
         logger.info(JSON.stringify({ ...processRequestObject(request), ...data }))
     }
 }
 
 var debug = function (data, request) {
-    if (loggerInit && config.enableLogger) {
+    if (loggerInitialized && config.enableLogger) {
         logger.debug(JSON.stringify({ ...processRequestObject(request), ...data }))
     }
 }
 
 var fatal = function (data, request) {
-    if (loggerInit && config.enableLogger) {
+    if (loggerInitialized && config.enableLogger) {
         logger.fatal(JSON.stringify({ ...processRequestObject(request), ...data }))
     }
 }
 
 var mark = function (data, request) {
-    if (loggerInit && config.enableLogger) {
+    if (loggerInitialized && config.enableLogger) {
         logger.mark(JSON.stringify({ ...processRequestObject(request), ...data }))
     }
 }
 
 var error = function (data, request) {
-    if (loggerInit && config.enableLogger) {
+    if (loggerInitialized && config.enableLogger) {
         logger.error(JSON.stringify({ ...processRequestObject(request), ...data }))
     }
 }
 
 var warn = function (data, request) {
-    if (loggerInit && config.enableLogger) {
+    if (loggerInitialized && config.enableLogger) {
         logger.warn(JSON.stringify({ ...processRequestObject(request), ...data }))
     }
 }
 
 var trace = function (data, request) {
-    if (loggerInit && config.enableLogger) {
+    if (loggerInitialized && config.enableLogger) {
         logger.trace(JSON.stringify({ ...processRequestObject(request), ...data }))
     }
 }
